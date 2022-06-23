2dbcacf847a8380f72d92d58792d1d5de32bf631
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-06-23 15:14:32 -0700

    fixed styling on tip form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import TipsForm from "./components/TipForm";
 
-function App() {
-	return (
-		<React.Fragment>
-			<Header />
-			<TipsForm />
-		</React.Fragment>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Header />
+				<TipsForm />
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -10,81 +10,87 @@ import {
 } from "reactstrap";
 import "./TipForm.css";
 
+import "../index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function TipsForm() {
 	return (
-		<div>
+		<div className="bg1 height100">
 			<Form className="row-content">
-				<FormGroup>
-					<Label for="tipTotal" className="float">
-						Total Credit Card Tips
-					</Label>
-
-					<Input id="tipTotal" name="tipTotal" for="text" />
-				</FormGroup>
-
-				<FormGroup>
-					<Label for="bb1" className="float">
-						Bar Back 1
-					</Label>
-
-					<Input id="bb1" name="bb1TipTotal" for="number" />
-				</FormGroup>
-
-				<FormGroup>
-					<Label for="bb2" className="float">
-						Bar Back 2
-					</Label>
-
-					<Input id="bb2" name="bb2TipTotal" for="number" />
-				</FormGroup>
-
-				<FormGroup>
-					<Label for="bar1Tips" className="float">
+				<FormGroup row>
+					<Label for="bar1Tips" sm={4}>
 						Bartender 1 Tips
 					</Label>
 
-					<Input id="bar1Tips" name="bar1TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar1Tips" name="bar1TipTotal" for="number" />
+					</Col>
 				</FormGroup>
 
-				<FormGroup>
-					<Label for="bar2Tips" className="float">
+				<FormGroup row>
+					<Label for="bar2Tips" sm={4}>
 						Bartender 2 Tips
 					</Label>
 
-					<Input id="bar2Tips" name="bar2TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar2Tips" name="bar2TipTotal" for="number" />
+					</Col>
 				</FormGroup>
 
-				<FormGroup>
-					<Label for="bar3Tips" className="float">
+				<FormGroup row>
+					<Label for="bar3Tips" sm={4}>
 						Bartender 3 Tips
 					</Label>
 
-					<Input id="bar3Tips" name="bar3TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar3Tips" name="bar3TipTotal" for="number" />
+					</Col>
 				</FormGroup>
 
-				<FormGroup>
-					<Label for="bar4Tips" className="float">
+				<FormGroup row>
+					<Label for="bar4Tips" sm={4}>
 						Bartender 4 Tips
 					</Label>
 
-					<Input id="bar4Tips" name="bar4TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar4Tips" name="bar4TipTotal" for="number" />
+					</Col>
 				</FormGroup>
 
-				<FormGroup>
-					<Label for="bar5Tips" className="float">
+				<FormGroup row>
+					<Label for="bar5Tips" sm={4}>
 						Bartender 5 Tips
 					</Label>
 
-					<Input id="bar5Tips" name="bar5TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar5Tips" name="bar5TipTotal" for="number" />
+					</Col>
 				</FormGroup>
 
-				<FormGroup>
-					<Label for="bar6Tips" className="float">
+				<FormGroup row>
+					<Label for="bar6Tips" sm={4}>
 						Bartender 6 Tips
 					</Label>
 
-					<Input id="bar6Tips" name="bar6TipTotal" for="number" />
+					<Col sm={8}>
+						<Input id="bar6Tips" name="bar6TipTotal" for="number" />
+					</Col>
 				</FormGroup>
+
+				<FormGroup row>
+					<Label for="tipTotal" sm={4}>
+						Total Credit Card Tips
+					</Label>
+
+					<Col sm={8}>
+						<Input id="tipTotal" name="tipTotal" for="text" />
+					</Col>
+				</FormGroup>
+				<Col className="btn-center">
+					<Button className="button-block bg2" size="lg">
+						Submit
+					</Button>
+				</Col>
 			</Form>
 		</div>
 	);
